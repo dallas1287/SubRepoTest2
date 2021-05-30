19beb7e625653e2a653733832756e0180b165da4
dallas1287 <jdthornton1@gmail.com>
2021-05-30 16:25:38 -0400

    minimum functional wasm with calls to decode frames

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
     // Create a worker for running Wasm code without blocking main thread.
     const worker = new Worker('worker.js');
 
@@ -12,6 +13,11 @@
     // Listen for messages back from worker and render to DOM.
     worker.onmessage = (e) => {
         const data = e.data;
+        if(!data)
+        {
+            console.log("no data available");
+            return;
+        }
         const results = document.getElementById('results');
         const ul = document.createElement('ul');
         const li = document.createElement('li');
@@ -19,28 +25,31 @@
         const li2 = document.createElement('li');
         li2.textContent = "duration: " + data.duration;
         const li3 = document.createElement('li');
-        li3.textContent = "streams: " + data.streams;
+        li3.textContent = "frame number: " + data.framePts;
+        const li4 = document.createElement('li');
+        li4.textContent = "is open: " + data.isOpen;
         ul.appendChild(li);
         ul.appendChild(li2);
         ul.appendChild(li3);
+        ul.appendChild(li4);
         results.appendChild(ul);
     }
 
     // Send file to worker.
     function onFileChange(event) {
         const file = input.files[0];
-        worker.postMessage([ file ]);
+        worker.postMessage(["open file", file ]);
     }
 
     function onPlayClicked(event) {
-        console.log('Play clicked');
+        worker.postMessage(["play"]);
     }
     function onPauseClicked(event) {
-        console.log('Pause clicked');
+        worker.postMessage(['pause']);
     }
     function onNextClicked(event) {
-        console.log('Next clicked');
+        worker.postMessage(['next frame']);
     }
     function onPrevClicked(event) {
-        console.log('Prev clicked');
+        worker.postMessage(['prev frame']);
     }
--- worker.js
+++ worker.js
@@ -1,25 +1,50 @@
 // Run this script as a Web Worker so it doesn't block the
 // browser's main thread.
+
 // See: index.html.
 onmessage = (e) => {
-    const file = e.data[0];
-    let data;
+    if(e.data === null)
+        return;
+    var instance = new Module.MediaPlayer();
+    var cmd = e.data[0];
+    if(cmd === 'open file')
+    {
+        const file = e.data[1];
 
-    // Create and mount FS work directory.
-    if (!FS.analyzePath('/work').exists) {
-        FS.mkdir('/work');
+        // Create and mount FS work directory.
+        if (!FS.analyzePath('/work').exists) {
+            FS.mkdir('/work');
+        }
+        FS.mount(WORKERFS, { files: [file] }, '/work');
+    
+        const instance = new Module.MediaPlayer();
+        // Run the Wasm function we exported.
+        const info = instance.OpenFile('/work/' + file.name);
+        // Post message back to main thread.
+        postMessage(info);
+    
+        // Unmount the work directory.
+        FS.unmount('/work');
     }
-    FS.mount(WORKERFS, { files: [file] }, '/work');
+    else if(cmd === 'play')
+    {
+        const frame = instance.Play();
+        postMessage(frame);
+    }
+    else if(cmd === 'pause')
+    {
+        instance.Pause();
+    }
+    else if(cmd === 'next frame')
+    {
+        instance.FrameStep();
+    }
+    else if(cmd === 'prev frame')
+    {
+        instance.RevFrameStep();
+    }
 
-    // Run the Wasm function we exported.
-    const info = Module.run('/work/' + file.name);
-    console.log(info);
-
-    // Post message back to main thread.
-    postMessage(info);
-
-    // Unmount the work directory.
-    FS.unmount('/work');
+    instance.delete();
 }
 
 // Import the Wasm loader generated from our Emscripten build.
